5a08f18561af24b5a85b3c0181fdbdcbc81185e9
DivyanshArya23 <divyansharya20@gmail.com>
2021-11-28 17:10:23 +0530

    Video 69

--- src/screens/TextScreen.jsx
+++ src/screens/TextScreen.jsx
@@ -2,21 +2,36 @@ import React, { useState } from "react";
 import { StyleSheet, Text, View, TextInput } from "react-native";
 
 const TextScreen = () => {
-  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const MAX_LENGTH = 5;
+  const MIN_LENGTH = 5;
+  const errors = {
+    max_length: `Password Cannot be more than ${MAX_LENGTH} characters`,
+    min_length: `Password Should be more than ${MIN_LENGTH} characters`,
+  };
   return (
     <View>
       <Text>Text Screen</Text>
-      <Text>Enter Name:</Text>
+      <Text>Enter Password:</Text>
       <TextInput
         style={styles.input}
         autoCapitalize="none"
         autoCorrect={false}
-        value={name}
+        value={password}
+        secureTextEntry={true}
         onChangeText={(text) => {
-          setName(text);
+          setPassword(text);
+          if (text.length < MIN_LENGTH) {
+            setError(errors.min_length);
+          } else {
+            setError("");
+          }
         }}
       />
-      <Text>My Name is : {name}</Text>
+      {error ? <Text style={styles.error}>{error}</Text> : null}
+      <Text>Password : {password}</Text>
     </View>
   );
 };
@@ -29,5 +44,8 @@ const styles = StyleSheet.create({
     padding: 10,
     borderColor: "black",
     borderWidth: 1,
+  },
+  error: {
+    color: "red",
   },
 });
